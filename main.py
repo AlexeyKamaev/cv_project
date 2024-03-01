bb51fec25c272956ad1eaa7cd720494fee2b21e6
Aleksey Kamaev <153755931+AlexeyKamaev@users.noreply.github.com>
2024-03-01 16:23:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ st.write('choose your option')
 
 with st.echo():
     st_lottie("https://lottie.host/d50d44da-8c5f-48da-af8b-57ad93b6e14c/h139wgXhFw.json")
-st.page_link("pages/autoenc.py", label="Denoiser DOCS", icon='📝')
+st.markdown(st.page_link("pages/autoenc.py", label="Denoiser DOCS", icon='📝'), unsafe_allow_html=True)
+
+
 with st.echo():
     st_lottie("https://lottie.host/embed/65695457-cee0-4a43-bad6-33ff5ab81798/n77e59fnDc.json")
 st.page_link("pages/myapp.py", label="Wind stations", icon='⚡')
