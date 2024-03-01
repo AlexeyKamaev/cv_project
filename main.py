a7ed40b49040b8f734d478c482de52aa1bf329f4
Aleksey Kamaev <153755931+AlexeyKamaev@users.noreply.github.com>
2024-03-01 16:30:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ st.title('📝 &  ⚡💨🍃🪫💡')
 
 st.write('choose your option')
 
-
+st.markdown(st.page_link("pages/autoenc.py", label="Denoiser DOCS", icon='📝'), unsafe_allow_html=True)
 with st.echo():
     st_lottie("https://lottie.host/d50d44da-8c5f-48da-af8b-57ad93b6e14c/h139wgXhFw.json")
-st.markdown(st.page_link("pages/autoenc.py", label="Denoiser DOCS", icon='📝'), unsafe_allow_html=True)
 
+st.page_link("pages/myapp.py", label="Wind stations", icon='⚡')
 with st.echo():
     st.lottie("https://lottie.host/a7d94a5a-41ff-428d-8760-1d6445f6a4dc/N7tAKQoF92.json")
     
-st.page_link("pages/myapp.py", label="Wind stations", icon='⚡')
 
-st.header(f'''made by: Alexey Kamaev & Marina Kochetova''')
+
+st.subheader(f'''made by: Alexey Kamaev & Marina Kochetova''')
