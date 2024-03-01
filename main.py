ca5a6d2d8b1ee84079acf2c80a77082e798723d4
Aleksey Kamaev <153755931+AlexeyKamaev@users.noreply.github.com>
2024-03-01 16:01:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import streamlit as st
 from streamlit_image_comparison import image_comparison
 
 # set page config
-st.set_page_config(page_title="Image-Comparison Example", layout="centered")
+# st.set_page_config(page_title="Image-Comparison Example", layout="centered")
 
 # render image-comparison
 image_comparison(
