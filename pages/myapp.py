a659453b9ce5309c90b628862ac904d2a9afbfe0
Aleksey Kamaev <153755931+AlexeyKamaev@users.noreply.github.com>
2024-03-01 15:09:06 +0300

    Update myapp.py

--- pages/myapp.py
+++ pages/myapp.py
@@ -10,9 +10,16 @@ from io import BytesIO
 from requests.models import MissingSchema
 from PIL import Image
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
 
-# Функция для загрузки изображения по URL и предварительной обработки
+def create_opencv_image_from_bytesio(img_bytesio, cv2_img_flag=1):
+    img_bytesio.seek(0)
+    img_array = np.asarray(bytearray(img_bytesio.read()), dtype=np.uint8)
+    return cv2.imdecode(img_array, cv2_img_flag)
+
+ssl._create_default_https_context = ssl._create_unverified_context
+# Ignore SSL certificate verification errors
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
 st.markdown("""
   <p style='text-align: center; font-size:36px'>
     Обнаружение ветрогенераторов с использованием YOLO
@@ -31,12 +38,6 @@ st.markdown("""
         }
     </style>
     """, unsafe_allow_html=True)
-
-
-def create_opencv_image_from_bytesio(img_bytesio, cv2_img_flag=1):
-    img_bytesio.seek(0)
-    img_array = np.asarray(bytearray(img_bytesio.read()), dtype=np.uint8)
-    return cv2.imdecode(img_array, cv2_img_flag)
 
 
 st.markdown("""
@@ -98,3 +99,54 @@ elif url:
             st.image(result[0].plot()[:, :, ::-1], caption="Результаты детекции", use_column_width=True)
     else:
         st.error("Ошибка при получении изображения", response.status_code)
+
+
+# Dataset information
+st.markdown("""
+  <p style='font-size:20px'>
+    <strong>Информация о датасете</strong><br>
+    Number of train images: 2643<br>
+    Number of test images: 130<br>
+    Number of valid images: 247
+  </p>
+""", unsafe_allow_html=True)
+
+# Model training information
+st.markdown("""
+  <p style='font-size:20px'>
+    <strong>Информация о модели</strong><br>
+    Модель YOLOv8n обучалась на 60 эпохах
+  </p>
+""", unsafe_allow_html=True)
+
+
+
+# Load and display graphs
+graph_path = "media_2/results.png"  # Replace with the actual path
+confusion_matrix_path = "media_2/confusion_matrix.png"  # Replace with the actual path
+
+# Load graphs
+graph_img = mpimg.imread(graph_path)
+confusion_matrix_img = mpimg.imread(confusion_matrix_path)
+
+# Display graphs
+st.markdown("""
+  <p style='font-size:20px'>
+    <strong>Графики</strong>
+  </p>
+""", unsafe_allow_html=True)
+st.image(graph_img, caption="Графики", use_column_width=True)
+
+st.markdown("""
+  <p style='font-size:20px'>
+    <strong>Confusion Matrix</strong>
+  </p>
+""", unsafe_allow_html=True)
+st.image(confusion_matrix_img, caption="Confusion Matrix", use_column_width=True)
+
+
+
+
+
+
+
